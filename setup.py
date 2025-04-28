00e0ebce08cb6b3f60b4975445123bcd3402d89f
Joel Nicolow <106400769+jnicolow@users.noreply.github.com>
2025-04-28 09:45:46 -1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Setup script for package
+
+Joel Nicolow, Coastal Research Collaborative, March 2025
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="geedownload",  # Package name (matches repo)
+    version="0.1",
+    packages=find_packages(),  # Automatically finds `geeutils/`
+    install_requires=[],  # Add dependencies if needed
+)
