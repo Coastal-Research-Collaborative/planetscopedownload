b0ab6ddc9227a34f6d6c6d5066903510c2129b56
Joel Nicolow <106400769+jnicolow@users.noreply.github.com>
2025-06-03 15:37:47 -1000

    Update __init__.py

--- planetscopedownload/__init__.py
+++ planetscopedownload/__init__.py
@@ -4,8 +4,8 @@ init script for planet scope api order class
 Joel Nicolow, Coastal Research Collaborative, April 2025
 """
 
-# this way makes it so all the functins from both scripts are just imported dirrectly from geedownload
-from planetscopedownload import planetscopedownload
+# this way makes it so all the functions from both scripts are just imported dirrectly from planetscopedownload
+from .planetscopedownload import *  # imports everything from geeutils.py under planetscopedownload module
 
 # or could do this to keep scripts as modules
 # import PlanetScopeOrdersAPI
